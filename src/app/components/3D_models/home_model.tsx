b7c81ff0fb64c3619e0f53b2424d9a4b8d6713cb
Sshrutisri123 <168011388+Sshrutisri123@users.noreply.github.com>
2025-04-08 23:48:14 +0530

    add links

--- src/app/components/3D_models/home_model.tsx
+++ src/app/components/3D_models/home_model.tsx
@@ -1,4 +1,3 @@
-
 import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -2,8 +2,30 @@
 
 import Link from 'next/link';
 import { ArrowUpRight } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const Footer = () => {
+  const [localTime, setLocalTime] = useState<string>("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const options: Intl.DateTimeFormatOptions = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true,
+        timeZoneName: 'short'
+      };
+      setLocalTime(now.toLocaleTimeString(undefined, options));
+    };
+
+    updateTime(); // initial call
+    const interval = setInterval(updateTime, 1000); // update every second
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
+
   return (
     <footer className="bg-black z-40 text-white pt-20 sm:pt-30 px-8 sm:px-20">
       <div className="w-full">
@@ -31,11 +53,10 @@ const Footer = () => {
 
         <div className="mt-6 flex sm:flex-row flex-col space-y-3 justify-between text-gray-500 text-xs">
           <span>2025 © Edition</span>
-          <span>Local Time: 2:31 AM GMT+5:30</span>
+          <span>Local Time: {localTime}</span>
           <div className="flex flex-col space-y-3 space-x-4">
-            
             <div className='flex space-x-4'>
-              <Link href="#" className="hover:text-white">LinkedIn</Link>
+              <Link href="https://www.linkedin.com/in/shrutisrivastava08/" className="hover:text-white">LinkedIn</Link>
               <Link href="#" className="hover:text-white">Threads</Link>
               <Link href="#" className="hover:text-white">Github</Link>
             </div>
@@ -47,13 +68,11 @@ const Footer = () => {
         </div>
 
         <div className='sm:hidden flex justify-center py-16'>
-          <button className="relative  px-8 py-4 text-5xl font-bold text-white bg-gradient-to-br from-white/20 to-white/5 backdrop-blur-lg border border-white/30 rounded-full shadow-lg shadow-white/10 hover:from-white/30 hover:to-white/10 hover:shadow-white/20 transition-all duration-300">
+          <button className="relative px-8 py-4 text-5xl font-bold text-white bg-gradient-to-br from-white/20 to-white/5 backdrop-blur-lg border border-white/30 rounded-full shadow-lg shadow-white/10 hover:from-white/30 hover:to-white/10 hover:shadow-white/20 transition-all duration-300">
             CONNECT
           </button>
         </div>
-
       </div>
-
     </footer>
   );
 };
